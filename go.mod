d33f8606f6b7cd40aabcd70b0a480993fb3e37f4
clayton-schneider <schneiderc2016@gmail.com>
2024-05-07 21:35:49 -0400

    start proj

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/clayton-schneider/tulip
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Metal struct {
+	Name string
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hello world")
+	})
+
+	mux.HandleFunc("/metals", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method is not allowed", 405)
+		}
+
+		var d []Metal
+		d = append(d, Metal{Name: "Aluminum"})
+		d = append(d, Metal{Name: "Copper"})
+		d = append(d, Metal{Name: "Silver"})
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusAccepted)
+		json.NewEncoder(w).Encode(d)
+	})
+
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4321"
+	}
+
+	srv := &http.Server{
+		Addr: ":"+port,
+		Handler: mux,
+	}
+
+	srv.ListenAndServe()
+}
